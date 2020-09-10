84c8fa54f48eeb73265f27a1fa1477d38dd121c1
Alice Tang <tang.ayt@gmail.com>
2020-09-10 23:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+  let sentArr = [];
+  for (let i = 0; i < arrMusicians.length; i++) {
+    sentArr.push(arrMusicians[i] + ' plays ' + arrInstruments[i]);
+  }
+  return sentArr;
+}
+
+function johnLennonFacts(arrFacts) {
+  while (arrFacts.length > 0) {
+    arrFacts[i] + '!!!';
+  }
+  return arrFacts;
+}
+
+function iLoveTheBeatles(num) {
+  do {
+    
+  }
+  while ()
+}
